f10eceb00f8c57a7582b6f92d086407a3e28996d
minnim1010 <pms1010123@daum.net>
2023-08-19 18:06:49 +0900

    값 타입 매핑
    city, street, zipcode를 Address 임베디드 타입으로 정의
     - Member, Delivery 엔티티에 적용

--- src/main/java/jpa/entity/Delivery.java
+++ src/main/java/jpa/entity/Delivery.java
@@ -9,9 +9,8 @@ public class Delivery {
     private Long id;
     @OneToOne(mappedBy = "delivery")
     private Order order;
-    private String city;
-    private String street;
-    private String zipcode;
+    @Embedded
+    private Address address;
     @Enumerated(EnumType.STRING)
     private DeliveryStatus status;
 
@@ -31,35 +30,21 @@ public class Delivery {
         this.order = order;
     }
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
     public DeliveryStatus getStatus() {
         return status;
     }
 
     public void setStatus(DeliveryStatus status) {
         this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Delivery{" +
+            "id=" + id +
+            ", order=" + order +
+            ", address=" + address +
+            ", status=" + status +
+            '}';
     }
 }
